2500a5ea3483c947705dddb4ec1e602f61a4da0c
0x7000 <rekreb81@gmail.com>
2021-01-23 16:14:15 +0300

    listeler

---
+++ 004_listeler.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def main():
+    # listeler ve özellikleri.
+    # boş bir liste için aşağıdaki kod ile başlanabilir
+    liste = []
+    # listeye eleman eklemek için .append(eleman) kullanılır örn.
+    liste.append("mesaj")
+    liste.append("yeni emsaj")
+    liste.append("son mesaj")
+    print(liste)
+    # tüm listeyi ekrana basacaktır
+    # tüm listeyi değilde liste elemanlarını sırayla bastırmak için
+    for i in liste:
+        print(i)
+    # listeden eleman silmek için
+    liste.remove("mesaj")
+    # burda listedeki mesaj isimli elemanı siler
+    print(liste)
+    # başka bir yöntem ile silmek
+    liste.remove(liste[0])
+    # listedeki sıfırıncı yani ilk eleman
+    print(liste)
+    # başka bir yöntem ile silmek
+    liste.pop(0)
+    # bu durumda listedeki silinen elemanın ne olduğuda yazılıcaktır.
+    print(liste)
+    # son halinde [] çıktısı ile karşılaşırız boş liste bu şekilde döner
+
+
+if __name__ == '__main__':
+    main()
