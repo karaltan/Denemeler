adee341b431c11da2c89b53a1c405324e2263465
0x7000 <rekreb81@gmail.com>
2021-02-28 16:54:37 +0300

    interpolasyon örneği.

--- 025_csipy_regresyon.py
+++ 025_csipy_regresyon.py
@@ -5,9 +5,9 @@ from scipy.interpolate import interp1d
 def main():
     X_nem = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     Y_isi = [15, 25, 35, 45, 55, 65, 75, 85, 95, 105]
-    intp = interp1d(X_nem, Y_isi)
+    intp = interp1d(X_nem, Y_isi, fill_value="extrapolate")
     print(intp.x, intp.y)
-    print(intp(33))  # X 33 olunca Y 38 olur.
+    print(intp(125))  # X 125 olunca Y 130 olur.
 
 
 if __name__ == '__main__':
